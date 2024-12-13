58055acd3b3ebd908b6be9fe42eda499f7e9b7ac
berkulutas <berkulutas2001@gmail.com>
2024-12-13 21:56:39 +0300

    inital implementation of covert channel

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,3 +1,7 @@
+from scapy.all import sniff
+from scapy.layers.l2 import Ether, ARP
+import time
+
 from CovertChannelBase import CovertChannelBase
 
 class MyCovertChannel(CovertChannelBase):
@@ -9,17 +13,46 @@ class MyCovertChannel(CovertChannelBase):
         """
         - You can edit __init__.
         """
-        pass
-    def send(self, log_file_name, parameter1, parameter2):
+        super().__init__()
+    def send(self, interface="eth0", idle_time=0.1, log_file_name="sending_log.log"):  # TODO add config? 
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        - Generates a random binary message and sends it using packet bursts over ARP.
         """
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
         
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
+        for bit in binary_message:
+            burst_size = 2 if bit == '1' else 1
+            for _ in range(burst_size):
+                packet = Ether(dst="ff:ff:ff:ff:ff:ff", type=0x0806) / ARP(pdst="172.18.0.3")  # TODO IP addr parameter or not ?
+
+                ### scapy.layers.l2.ARP(_pkt, /, *, hwtype=1, ptype=2048, hwlen=None, plen=None, op=1, hwsrc=None, psrc=None, hwdst=None, pdst=None)
+                print(packet.show())
+                super().send(packet, interface)
+            time.sleep(idle_time)  # Idle time between bursts
+
+    def receive(self, interface="eth0", timeout=10, idle_threshold=0.05, log_file_name="received_log.log"):
         """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        - Captures ARP packets, decodes bursts into a binary message.
         """
-        self.log_message("", log_file_name)
+        packets = sniff(iface=interface, timeout=timeout, ) # filter="arp")
+        print(packets.summary())
+        message = ""
+        burst_count = 0
+        last_packet_time = None
+
+        for packet in packets:
+            print(packet)
+            # if ARP in packet:
+            current_time = time.time()
+            if last_packet_time and (current_time - last_packet_time > idle_threshold):
+                # End of a burst
+                if burst_count == 2:
+                    message += '1'
+                elif burst_count == 1:
+                    message += '0'
+                burst_count = 0
+            burst_count += 1
+            last_packet_time = current_time
+
+        # Log the received message
+        self.log_message(message, log_file_name)
